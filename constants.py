4dc1505343a7d4cde88ca613f9bfa95c41fe5232
kylemckibben <kylemckibben@proton.me>
2026-03-15 18:20:42 -0400

    Add player
    * Create player class with movement controls implemented
    * Implement groups for updatable and drawable objects

--- constants.py
+++ constants.py
@@ -1,2 +1,12 @@
+# screen settings
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+# player settings
+PLAYER_RADIUS = 20
+PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
+
+# other constants
+LINE_WIDTH = 2
+
--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+    # groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)    
+
     while True:
         log_state()
 
@@ -19,7 +28,11 @@ def main():
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
