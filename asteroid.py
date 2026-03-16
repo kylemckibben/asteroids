df4dbe7ee6048341189938c6937261a60b3a7374
kylemckibben <kylemckibben@proton.me>
2026-03-15 21:43:10 -0400

    Add asteroid splitting
    * asteroids now split unless alreayd the smallest size, then they are destroyed
    * sizes are large, medium, small

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,9 @@
 import pygame
+import random
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +14,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        velocity_new_asteroid1 = self.velocity.rotate(angle)
+        velocity_new_asteroid2 = self.velocity.rotate(-angle)
+        new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid1 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+        new_asteroid2 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+        new_asteroid1.velocity = velocity_new_asteroid1 * 1.2
+        new_asteroid2.velocity = velocity_new_asteroid2 * 1.2
